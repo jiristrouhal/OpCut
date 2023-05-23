1c252b936827e4b980d77945bfc28d06c7ab213a
M3ezon <MznStrouhal@gmail.com>
2023-05-23 14:38:30 +0200

    Catch matching stock and lengths at the beginning of the sorting in sortall.py

--- sortall.py
+++ sortall.py
@@ -4,24 +4,32 @@ import dataclasses
 
 @dataclasses.dataclass
 class Ordered_Stock:
-	_length:int
+	length:int
 	_count:int
-	
+
 	def take(self)->int: 
 		if self._count>0: 
 			self._count -= 1
-			return self._length
+			return self.length
 		return 0
 		
 
-def mincutsort(lengths:List[int],stock:Dict[int,int])->Tuple[List[int],List[int]]:
-	sorted_lengths:List[int] = lengths.copy()
-	ord_stock:List[Ordered_Stock] = __prepare_stock_for_taking(stock)
-	sorted_stock:List[int] = list()
-	while True:
-		added_stock = ord_stock[0].take()
-		if added_stock==0: break
-		sorted_stock.append(added_stock)
+def mincutsort(lengths:List[int],stock_dict:Dict[int,int])->Tuple[List[int],List[int]]:
+	stock:List[Ordered_Stock] = __prepare_stock_for_taking(stock_dict)
+
+	matching_lengths = __pick_lengths_and_stock_of_same_length(lengths,stock)
+	sorted_lengths = matching_lengths.copy() + lengths
+	sorted_stock = matching_lengths.copy()
+
+	k=0
+	while k<len(stock):
+		next_item_length = stock[k].take()
+		if next_item_length==0: 
+			k+=1
+			continue
+		else:
+			sorted_stock.append(next_item_length)
+
 	return sorted_lengths, sorted_stock
 	
 
@@ -30,3 +38,16 @@ def __prepare_stock_for_taking(stock:Dict[int,int])->List[Ordered_Stock]:
 	for length,count in stock.items():
 		ordered_stock.append(Ordered_Stock(length,count))
 	return ordered_stock
+
+
+def __pick_lengths_and_stock_of_same_length(
+	lengths:List[int],
+	stock:List[Ordered_Stock]
+	)->List[int]:
+
+	matches:List[int] = list()
+	for item in stock:
+		if (item.length in lengths) and (item.take()>0): 
+			lengths.remove(item.length)
+			matches.append(item.length)
+	return matches
--- tests/test_sortall.py
+++ tests/test_sortall.py
@@ -8,10 +8,20 @@ class Test_Single_Length(unittest.TestCase):
 	def test_for_single_length_and_stock_no_sorting_happens(self)->None:
 		lengths:List[int] = [100]
 		stock:Dict[int,int] = {100:1}
-		sorted_lenghts, sorted_stock = sortall.mincutsort(lengths,stock)
-		self.assertListEqual(lengths,sorted_lenghts)
+		sorted_lengths, sorted_stock = sortall.mincutsort(lengths,stock)
+		print(sorted_lengths)
+		self.assertListEqual(sorted_lengths,[100])
 		self.assertListEqual(sorted_stock,[100])
 
+
+class Test_Two_Lengths(unittest.TestCase):
+	
+	def test_one_length_matching_stock_length_is_moved_to_coincide_with_the_stock(self):
+		lengths = [100,150]
+		stock = {150:2}
+		sorted_lengths, sorted_stock = sortall.mincutsort(lengths,stock)
+		self.assertListEqual(sorted_lengths,[150,100])
+		self.assertListEqual(sorted_stock,[150,150])
 
 
 
