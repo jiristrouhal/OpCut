323dd1a3ce9fa1cf7fa055fb2ed90146712ac08f
M3ezon <MznStrouhal@gmail.com>
2023-05-23 19:26:55 +0200

    Make cut function return pieces grouped by length and stock

--- cut.py
+++ cut.py
@@ -1,3 +1,63 @@
-from typing import List
+from typing import List, Protocol, Tuple
+import dataclasses
+
+
+@dataclasses.dataclass
+class Length_P(Protocol):
+	length:int
+	id:int
+
+
+@dataclasses.dataclass
+class Length:
+	length:int
+	id:int=0
+
+
+@dataclasses.dataclass
+class Cutted_Length:
+	original:Length
+	pieces:List[int]
+
+
+@dataclasses.dataclass
+class Cutted_Stock:
+	original:int
+	pieces:List[int]
+
+
+def cut(lengths:List[Length],stock:List[int])->Tuple[List[Cutted_Length],List[Cutted_Stock]]:
+	cutted_lengths:List[Cutted_Length] = list()
+	cutted_stock:List[Cutted_Stock] = list()
+
+	for l in lengths: cutted_lengths.append(Cutted_Length(l,[]))
+	for s in stock: cutted_stock.append(Cutted_Stock(s,[]))
+
+	i,j = 0,0
+	dist_to_length_end = lengths[i].length
+	dist_to_stock_end = stock[j]
+
+	m,n = len(lengths), len(stock)
+
+	while not (i==m or j==n):
+		if dist_to_length_end<dist_to_stock_end:
+			cutted_lengths[i].pieces.append(dist_to_length_end)
+			cutted_stock[j].pieces.append(dist_to_length_end)
+			i += 1
+			dist_to_stock_end -= dist_to_length_end
+			if i<m: dist_to_length_end = lengths[i].length
+			else: cutted_stock[j].pieces.append(dist_to_stock_end)
+		else:
+			cutted_lengths[i].pieces.append(dist_to_stock_end)
+			cutted_stock[j].pieces.append(dist_to_stock_end)
+			j += 1
+			dist_to_length_end -= dist_to_stock_end
+			if j<n: dist_to_stock_end = stock[j]
+			if dist_to_length_end==0: 
+				i += 1
+				if j<m: dist_to_length_end = lengths[i].length
+
+	return cutted_lengths, cutted_stock
+
 
 
--- tests/test_cut.py
+++ tests/test_cut.py
@@ -1,10 +1,72 @@
 import unittest
 import cut
+from cut import Length, Cutted_Length, Cutted_Stock
 
 
-class Test_Dummy(unittest.TestCase):
+class Test_Single_Lengths(unittest.TestCase):
 
-	pass
+	def test_stock_of_same_length_is_not_cutted(self):
+		lengths = [Length(100)]
+		stock = [100]
+		cutted_lengths, cutted_stock = cut.cut(lengths,stock)
+		self.assertListEqual(
+			cutted_lengths,
+			[Cutted_Length(Length(100),[100])]
+		)
+		self.assertListEqual(
+			cutted_stock,
+			[Cutted_Stock(100,[100])]
+		)
+	
+	def test_stock_of_twice_length_is_cutted_in_half(self):
+		lengths = [Length(100)]
+		stock = [200]
+		cutted_lengths, cutted_stock = cut.cut(lengths,stock)
+		self.assertListEqual(
+			cutted_lengths,
+			[Cutted_Length(Length(100,0),[100])]
+		)
+		self.assertListEqual(
+			cutted_stock,
+			[Cutted_Stock(200,[100,100])]
+		)
+
+
+class Test_Two_Lengths(unittest.TestCase):
+	
+	def test_matching_ends(self):
+		lengths = [Length(150), Length(150)]
+		stock = [150,150]
+		cutted_lengths, cutted_stock = cut.cut(lengths,stock)
+		self.assertListEqual(
+			cutted_lengths,
+			[Cutted_Length(Length(150),[150]), 
+    		Cutted_Length(Length(150),[150])]
+		)
+		self.assertListEqual(
+			cutted_stock,
+			[
+				Cutted_Stock(150,[150]),
+				Cutted_Stock(150,[150])
+			]
+		)
+	
+	def test_not_matching_ends(self):
+		lengths = [Length(200), Length(100)]
+		stock = [150,150]
+		cutted_lengths, cutted_stock = cut.cut(lengths,stock)
+		self.assertListEqual(
+			cutted_lengths,
+			[Cutted_Length(Length(200),[150,50]), 
+    		Cutted_Length(Length(100),[100])]
+		)
+		self.assertListEqual(
+			cutted_stock,
+			[
+				Cutted_Stock(150,[150]),
+				Cutted_Stock(150,[50,100])
+			]
+		)
 
 
 if __name__=='__main__': unittest.main()
