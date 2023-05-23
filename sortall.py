6dbda9c6cecb825910d8e6acc1bbe32a757b2d15
M3ezon <MznStrouhal@gmail.com>
2023-05-23 15:04:20 +0200

    Store original ids of sorted lengths in sortall.py

--- sortall.py
+++ sortall.py
@@ -3,6 +3,12 @@ import dataclasses
 
 
 @dataclasses.dataclass
+class Length:
+	length:int
+	id:int
+
+
+@dataclasses.dataclass
 class Ordered_Stock:
 	length:int
 	_count:int
@@ -12,14 +18,22 @@ class Ordered_Stock:
 			self._count -= 1
 			return self.length
 		return 0
+	
+
+class NotEnoughStockItems(Exception): pass
 		
 
-def mincutsort(lengths:List[int],stock_dict:Dict[int,int])->Tuple[List[int],List[int]]:
+def mincutsort(lengths_list:List[int],stock_dict:Dict[int,int])->Tuple[List[Length],List[int]]:
+	# The lengths list is converted into list of Length objects, which keep their original id
+	# stored as an attribute for later use.
+	__check_enough_stock_items(sum(lengths_list),stock_dict)
+
+	lengths:List[Length] = __prepare_lengths_for_sorting(lengths_list)
 	stock:List[Ordered_Stock] = __prepare_stock_for_taking(stock_dict)
 
 	matching_lengths = __pick_lengths_and_stock_of_same_length(lengths,stock)
 	sorted_lengths = matching_lengths.copy() + lengths
-	sorted_stock = matching_lengths.copy()
+	sorted_stock = [l.length for l in matching_lengths]
 
 	k=0
 	while k<len(stock):
@@ -31,7 +45,16 @@ def mincutsort(lengths:List[int],stock_dict:Dict[int,int])->Tuple[List[int],List
 			sorted_stock.append(next_item_length)
 
 	return sorted_lengths, sorted_stock
+
+
+def __check_enough_stock_items(total_length, stock:Dict[int,int]):
+	stock_length_sum = 0
+	for length,count in stock.items(): stock_length_sum += length*count
+	if stock_length_sum<total_length: raise NotEnoughStockItems
 	
+
+def __prepare_lengths_for_sorting(lengths:List[int])->List[Length]:
+	return [Length(lengths[i],i) for i in range(len(lengths))]
 
 def __prepare_stock_for_taking(stock:Dict[int,int])->List[Ordered_Stock]:
 	ordered_stock:List[Ordered_Stock] = list()
@@ -41,13 +64,14 @@ def __prepare_stock_for_taking(stock:Dict[int,int])->List[Ordered_Stock]:
 
 
 def __pick_lengths_and_stock_of_same_length(
-	lengths:List[int],
+	lengths:List[Length],
 	stock:List[Ordered_Stock]
-	)->List[int]:
+	)->List[Length]:
 
-	matches:List[int] = list()
-	for item in stock:
-		if (item.length in lengths) and (item.take()>0): 
-			lengths.remove(item.length)
-			matches.append(item.length)
+	matches:List[Length] = list()
+	for stock_item in stock:
+		for length_item in lengths:
+			if (stock_item.length==length_item.length) and (stock_item.take()>0): 
+				lengths.remove(length_item)
+				matches.append(length_item)
 	return matches
--- tests/test_sortall.py
+++ tests/test_sortall.py
@@ -1,5 +1,6 @@
 import unittest
 import sortall
+from sortall import Length, NotEnoughStockItems
 from typing import List, Dict
 
 
@@ -9,19 +10,27 @@ class Test_Single_Length(unittest.TestCase):
 		lengths:List[int] = [100]
 		stock:Dict[int,int] = {100:1}
 		sorted_lengths, sorted_stock = sortall.mincutsort(lengths,stock)
-		print(sorted_lengths)
-		self.assertListEqual(sorted_lengths,[100])
+
+		self.assertListEqual(sorted_lengths,[Length(100,0)])
 		self.assertListEqual(sorted_stock,[100])
+
+	def test_sum_of_stock_lengths_raises_exception(self)->None:
+		with self.assertRaises(NotEnoughStockItems):
+			lengths:List[int] = [100]
+			stock:Dict[int,int] = {}
+			sortall.mincutsort(lengths,stock)
 
 
 class Test_Two_Lengths(unittest.TestCase):
 	
-	def test_one_length_matching_stock_length_is_moved_to_coincide_with_the_stock(self):
+	def test_length_matching_stock_length_is_moved_to_coincide_with_the_stock(self):
 		lengths = [100,150]
 		stock = {150:2}
 		sorted_lengths, sorted_stock = sortall.mincutsort(lengths,stock)
-		self.assertListEqual(sorted_lengths,[150,100])
+		self.assertListEqual(sorted_lengths,[Length(150,1),Length(100,0)])
 		self.assertListEqual(sorted_stock,[150,150])
+
+	
 
 
 
